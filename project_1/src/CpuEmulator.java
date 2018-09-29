99837e1de9dba941046d6d583d27843d579c495f
sengfung27 <sengfung27@hotmail.com>
2018-09-29 16:35:20 -0400

    before testing

--- project_1/src/CpuEmulator.java
+++ project_1/src/CpuEmulator.java
@@ -1,10 +1,44 @@
 import pcb_package.PCB;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.ServerSocket;
+import java.io.IOException;
 
 public class CpuEmulator
 {
-    public static void main(String[] args)
-    {
-        System.out.println("Hello, World");
+    public static final int port = 6666;
+    private ServerSocket ss = null;
 
+    public void  runServer() throws IOException{
+        ss = new ServerSocket(port);
+        System.out.println("Connection Established.");
+         
+            Socket socket = ss.accept();
+            ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+            ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+            PCB process_pcb = (PCB) ois.readObject();
+            System.out.println("CPU: Exec "
+             + process.program_name + " "
+             + Integer.toString(process_pcb.est_remain_time));
+            minusCycle(process_pcb);
+            
+            oos.writeObject(process_pcb);
+            socket.close();       
+    }
+
+    private void minusCycle(PCB process) {
+        if(process.est_remain_time > 200)
+        {
+            process.est_remain_time -= 200;
+        }
+        else 
+        {
+            process.est_remain_time = 0;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        new CpuEmulator().runServer();
     }
 }
--- project_1/src/Scheduler.java
+++ project_1/src/Scheduler.java
@@ -3,18 +3,23 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.IOException;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Queue;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.ServerSocket;
 
 public class Scheduler
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws UnknownHostException,
+            IOException, ClassNotFoundException
     {
 
         ArrayList<PCB> processes = new ArrayList<PCB>();
-
+        Queue<PCB> scheduler = new LinkedList<>();
         try (BufferedReader br = new BufferedReader(new FileReader("processes.txt")))
         {
 
@@ -38,8 +43,45 @@ public class Scheduler
         for(PCB i : processes)
         {
             System.out.println(i.program_name);
+            System.out.println("Now pushing to Queue.");
+            scheduler.add(i);
+            System.out.println(scheduler.peek());
         }
+        System.out.println("welcome client");
+        Socket socket = new Socket("127.0.0.1", 6666);
+        System.out.println("Client connected");
+        while (scheduler.peek() != null)
+        {
+            ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+            oos.writeObject(scheduler.remove());
+            ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
 
-        
+            PCB return_process = (PCB) ois.readObject();
+
+            if (return_process.est_remain_time == 0)
+            {
+                System.out.println("Scheduler: Process " + return_process.PID + " " + return_process.program_name + " Completed.");
+            }
+            else
+            {
+                scheduler.add(return_process);
+            }
+        }
+        socket.close();
+    }
+
+    public void printScheduler(PCB process)
+    {
+        System.out.println();
+        System.out.println();
+
+        System.out.println();
+
+        System.out.println();
+
+        System.out.println();
+
+        System.out.println();
+
     }
 }
