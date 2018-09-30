978a268a6bc8eb8d36f7469fe7c185f372132f74
sengfung27 <sengfung27@hotmail.com>
2018-09-30 18:31:29 -0400

    remove comment

--- project_1/PriorityComparator.java
+++ project_1/PriorityComparator.java
@@ -5,10 +5,7 @@ public class PriorityComparator implements Comparator<PCB>
     @Override
     public int compare(PCB x, PCB y)
     {
-        // Assume neither string is null. Real code should
-        // probably be more robust
-        // You could also just return x.length() - y.length(),
-        // which would be more efficient.
+
         if (x.priority < y.priority)
         {
             return -1;
