718cb918ecccefca6306e7fb3c933dfb10b23042
sengfung27 <sengfung27@hotmail.com>
2018-10-06 16:47:05 -0400

    add shell script

---
+++ project_1/compilescript.sh
@@ -0,0 +1,15 @@
+#! /usr/bin/bash
+
+javac PCB.java PriorityComparator.java Scheduler.java CpuEmulator.java &&
+jar -cmf Scheduler.mf Scheduler.jar PCB.class PriorityComparator.class Scheduler.class &&
+jar -cmf CpuEmulator.mf CpuEmulator.jar PCB.class CpuEmulator.class &&
+gcc -o init init.c &&
+
+
+
+
+## Default: Round Robin Scheduling
+./init
+
+## Optional: Priority First Scheduling
+./init priority
