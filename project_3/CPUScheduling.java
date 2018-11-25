cbbd38c3c2d54fa375484f73c54aa0d2f3eb9de0
sengfung27 <sengfung27@hotmail.com>
2018-11-25 16:27:49 -0500

    shortest job next scheduling

--- project_3/CPUScheduling.java
+++ project_3/CPUScheduling.java
@@ -12,119 +12,119 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 
-class CPUScheduling
-{
-    // Read file and add into a scheduler 
-    public static Queue<PCB> readFile()
-    {
+class CPUScheduling {
+    // Read file and add into a scheduler
+    public static Queue<PCB> readFile() {
         Queue<PCB> scheduler = new LinkedList<>();
-        try (BufferedReader br = new BufferedReader(new FileReader("processes_3.txt")))
-        {
-            
+        try (BufferedReader br = new BufferedReader(new FileReader("processes_3.txt"))) {
+
             // Read file line by line
             String line = "";
-            while ((line = br.readLine()) != null)
-            {
+            while ((line = br.readLine()) != null) {
                 scheduler.add(new PCB(line));
             }
 
-            
-        }
-        catch (IOException e)
-        {
+        } catch (IOException e) {
             e.printStackTrace();
 
         }
         return scheduler;
-        
+
     }
 
     // First Come First Serve Scheduling
-    public static void firstComeFirstServe(Queue<PCB> schedulerTask)
-    {
-        int context_switches = 0; // number times of context switch 
-        int flag = 1;        // indicate cpu status. 0 = running, 1 = ready. One process at a time
+    public static void scheduling(Queue<PCB> schedulerTask, PriorityQueue<PCB> scheduler) {
+        int context_switches = 0; // number times of context switch
+        int flag = 1; // indicate cpu status. 0 = running, 1 = ready. One process at a time
 
         int currentTime = 0; // clock, counter
-        ArrayList<PCB> finishedPcbList = new ArrayList<PCB>();
-        Queue<PCB> scheduler = new LinkedList<>();  // scheduling algorithm queue
-        PCB currentPcb = schedulerTask.remove();    // the first PCB in Scheduler queue
+        ArrayList<PCB> finishedPcbList = new ArrayList<PCB>(); // list of finished PCB for calculation
 
-        PCB cpuPcb = null;                          // Running cpu PCB 
-        
-        while (schedulerTask.peek() != null || scheduler.peek() != null || flag == 0)
-        {
-            if (currentTime == currentPcb.arrival_time)
-            {
+        PCB currentPcb = schedulerTask.remove(); // the first PCB in Scheduler queue
+
+        PCB cpuPcb = null; // Running cpu PCB
+
+        // keep cycle if both schedulerTask and scheduler has at least one process and cpu is running
+        while (schedulerTask.peek() != null || scheduler.peek() != null || flag == 0) {
+            // PCB added into scheduler when it's time
+            if (currentTime == currentPcb.arrival_time) {
                 scheduler.add(currentPcb);
-                System.out.println("Scheduler: " + currentPcb.program_name 
-                + " is entered the scheduling Queue at time " + currentTime);
-                if (schedulerTask.peek() != null)
-                {
+                System.out.println("Scheduler: " + currentPcb.program_name + " is entered the scheduling Queue at time "
+                        + currentTime);
+                if (schedulerTask.peek() != null) {
                     currentPcb = schedulerTask.remove();
                 }
 
                 // first process to come in
-                if (flag == 1)
-                {
+                if (flag == 1) {
                     cpuPcb = scheduler.remove();
-                    System.out.println("Cpu Emulator: Cpu is ready to process " + cpuPcb.program_name 
-                    + " at time " + currentTime);
+                    System.out.println(
+                            "Cpu Emulator: Cpu is ready to process " + cpuPcb.program_name + " at time " + currentTime);
                     flag = 0;
                     cpuPcb.is_first_time = false;
                 }
 
             }
-            
-            // CPU Emulator is running a process
-            if (flag == 0)
-            {
 
-                // decrement the process if it's not 0 
-                if (cpuPcb.est_remain_time != 0)
-                {
+            // CPU Emulator is running a process
+            if (flag == 0) {
+
+                // decrement the process in cpu if it's not 0
+                if (cpuPcb.est_remain_time != 0) {
                     cpuPcb.est_remain_time -= 1;
-                    scheduler = decrementScheduler(scheduler, 1);
+                    cpuPcb.turnaround_time += 1;
+                    // increment 1 unit of wait time and turn around time in scheduler process.
+                    if (scheduler.peek() != null) {
+                        for (PCB waiting_block : scheduler) 
+                        {
+                            waiting_block.total_waiting_time += 1; // 1 unit wait time for process in scheduler
+                            if (!waiting_block.is_first_time) {
+                                waiting_block.turnaround_time += 1; // 1 unit turnaround_time for process in scheduler
+                            }
+                        }
+                    }
                 }
 
                 // One process finished in cpu
-                else
-                {
-                    finishedPcbList.add(cpuPcb);
+                else {
                     
-                    System.out.println("Cpu Emulator: " + cpuPcb.program_name +
-                                    " is finished at time " + currentTime +
-                                    ". Waiting Time: "
-                                    + cpuPcb.total_waiting_time 
-                                    + ". Estimate Remaining Time: "
-                                    + cpuPcb.est_remain_time);
-                    // Scheduler has at least one process 
-                    if (scheduler.peek() != null)
-                    {
-                        context_switches += 1;    // context switch to next process
-                        currentTime += 2;         // add 2 units context switch
-                        cpuPcb = scheduler.remove();  // load the next PCB to cpu 
-                        System.out.println("Cpu Emulator: Cpu is ready to process " + cpuPcb.program_name 
-                        + " at time " + currentTime + ". Waited time: " + cpuPcb.total_waiting_time);
-                        cpuPcb.is_first_time = false;  // no longer first time to load in cpu
-                        cpuPcb.est_remain_time -= 1;   // decrement estimate remaining time
-                        scheduler = decrementScheduler(scheduler, 3); // decrement other property in scheduler
+                    finishedPcbList.add(cpuPcb);
+
+                    System.out.println("Cpu Emulator: " + cpuPcb.program_name + " is finished at time " + currentTime
+                            + ". Waiting Time: " + cpuPcb.total_waiting_time + ". Estimate Remaining Time: "
+                            + cpuPcb.est_remain_time);
+                    // Scheduler has at least one process
+                    if (scheduler.peek() != null) {
+                        context_switches += 1; // context switch to next process
+                        currentTime += 2; // add 2 units context switch
+                        cpuPcb = scheduler.remove(); // load the next PCB to cpu
+                        System.out.println("Cpu Emulator: Cpu is ready to process " + cpuPcb.program_name + " at time "
+                                + currentTime + ". Waited time: " + cpuPcb.total_waiting_time);
+                        cpuPcb.is_first_time = false; // no longer the first time to load in cpu
+                        cpuPcb.est_remain_time -= 1; // decrement estimate remaining time
+                        // decrement other property in scheduler
+                        if (scheduler.peek() != null) {
+                            for (PCB waiting_block : scheduler) 
+                            {
+                                waiting_block.total_waiting_time += 2; // 2 unit wait time for process in scheduler
+                                if (!waiting_block.is_first_time) {
+                                    waiting_block.turnaround_time += 2; // 2 unit turnaround_time for process in scheduler
+                                }
+                            }
+                        }
                     }
 
-                    // finished all the process and print it out
-                    else
-                    {
-                        
-                        flag = 1;
+                    // finished all the process in cpu emulator and scheduler and print it out
+                    else {
+
+                        flag = 1; 
                         int sum_wait_time = 0;
                         int sum_turnaround_time = 0;
                         int longest_wait_time = 0;
-                        for (PCB finished_block : finishedPcbList)
-                        {
+                        for (PCB finished_block : finishedPcbList) {
                             sum_wait_time += finished_block.total_waiting_time;
                             sum_turnaround_time += finished_block.turnaround_time;
-                            if (longest_wait_time < finished_block.total_waiting_time)
-                            {
+                            if (longest_wait_time < finished_block.total_waiting_time) {
                                 longest_wait_time = finished_block.total_waiting_time;
                             }
                         }
@@ -132,8 +132,9 @@ class CPUScheduling
                         System.out.println("Total time used: " + currentTime);
                         System.out.println("Context switch in total: " + context_switches);
                         System.out.println("Longest wait time: " + longest_wait_time);
-                        System.out.println("Average wait time: " + sum_wait_time/14 );
-                        System.out.println("Average turnaround time: " + sum_turnaround_time/14 );
+                        System.out.println("Average wait time: " + sum_wait_time / 14);
+                        System.out.println("Average turnaround time: " + sum_turnaround_time / 14);
+                        System.out.println("---------------------------------------------------------------------------------------------");
                     }
                 }
             }
@@ -141,35 +142,19 @@ class CPUScheduling
         }
     }
 
-    // decrement other property 
-    public static Queue<PCB> decrementScheduler(Queue<PCB> scheduler, int value)
-    {
-        Queue<PCB> new_scheduler = new LinkedList<>();
-        if (scheduler.peek() == null)
-        {
-            new_scheduler = scheduler;
-            return new_scheduler;
+    public static void main(String[] args) {
+        Queue<PCB> schedulerTask = readFile();
+
+        if (Integer.parseInt(args[0]) == 1) {
+            PriorityQueue<PCB> firstComeFirstServe = new PriorityQueue<PCB>(20, new FirstComeFirstServeComparator()); 
+            System.out.println("First Come First Serve Scheduling - no pre-emption");          
+            scheduling(schedulerTask, firstComeFirstServe);
+
+        } 
+        else if (Integer.parseInt(args[0]) == 2) {
+            PriorityQueue<PCB> shortestJobNext = new PriorityQueue<PCB>(20, new ShortestJobComparator()); 
+            System.out.println("Shortest Job Next Scheduling - no pre-emption");          
+            scheduling(schedulerTask, shortestJobNext);
         }
-        for (PCB waiting_block : scheduler)
-        {
-            waiting_block.total_waiting_time += value;  // 1 + 2 unit context switch
-            if (!waiting_block.is_first_time)
-            {
-                waiting_block.turnaround_time += value;  // 1 + 2 unit context switch
-            }
-            new_scheduler.add(waiting_block);
-        }
-
-        return new_scheduler;
-    }
-
-
-    public static void main(String [] args)
-    {
-        
-        Queue<PCB> scheduler = readFile();
-
-        firstComeFirstServe(scheduler);
-        
     }
 }
--- project_3/compilescript.sh
+++ project_3/compilescript.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
 
-javac PCB.java CPUScheduling.java &&
-jar -cmf CPUScheduling.mf CPUScheduling.jar PCB.class CPUScheduling.class &&
-java -cp CPUScheduling.jar CPUScheduling first
+javac PCB.java ShortestJobComparator.java CPUScheduling.java FirstComeFirstServeComparator.java &&
+jar -cmf CPUScheduling.mf CPUScheduling.jar PCB.class ShortestJobComparator.class CPUScheduling.class FirstComeFirstServeComparator.class  &&
+
+# first come first serve algorithm
+java -cp CPUScheduling.jar CPUScheduling 1
+
+# shortest job next algorithm
+java -cp CPUScheduling.jar CPUScheduling 2
 
